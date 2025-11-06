40472fa5f44cd2674a490624914a7e1f3506eeb8
canhle1993 <aplevancanh1993@gmail.com>
2025-11-06 16:24:10 +0700

    update new

--- pharmatech-frontend/PharmaTech/src/app/user/pages/profile/profile.component.ts
+++ pharmatech-frontend/PharmaTech/src/app/user/pages/profile/profile.component.ts
@@ -16,7 +16,7 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./profile.component.css'],
   providers: [MessageService],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
   account: Account | null = null;
   loading = false;
   isEditing = false;
@@ -155,5 +155,54 @@ export class ProfileComponent implements OnInit {
         detail: 'Failed to update profile!',
       });
     }
+  }
+
+  ngAfterViewInit() {
+    // --- CSS ---
+    const cssFiles = [
+      'assets/css/vendor/bootstrap.min.css',
+      'assets/css/vendor/lastudioicons.css',
+      'assets/css/vendor/dliconoutline.css',
+      'assets/css/animate.min.css',
+      'assets/css/swiper-bundle.min.css',
+      'assets/css/ion.rangeSlider.min.css',
+      'assets/css/lightgallery-bundle.min.css',
+      'assets/css/magnific-popup.css',
+      'assets/css/style.css',
+    ];
+    cssFiles.forEach((href) => {
+      const link = this.renderer.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = href;
+      this.renderer.appendChild(document.head, link);
+    });
+
+    const fontLink = this.renderer.createElement('link');
+    fontLink.rel = 'stylesheet';
+    fontLink.href =
+      'https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap';
+    this.renderer.appendChild(document.head, fontLink);
+
+    // --- JS ---
+    const jsFiles = [
+      'assets/js/vendor/modernizr-3.11.7.min.js',
+      'assets/js/vendor/jquery-migrate-3.3.2.min.js',
+      'assets/js/countdown.min.js',
+      'assets/js/ajax.js',
+      'assets/js/jquery.validate.min.js',
+      'assets/js/vendor/jquery-3.6.0.min.js',
+      'assets/js/vendor/bootstrap.bundle.min.js',
+      'assets/js/swiper-bundle.min.js',
+      'assets/js/ion.rangeSlider.min.js',
+      'assets/js/lightgallery.min.js',
+      'assets/js/jquery.magnific-popup.min.js',
+      'assets/js/main.js',
+    ];
+    jsFiles.forEach((src) => {
+      const script = this.renderer.createElement('script');
+      script.src = src;
+      script.type = 'text/javascript';
+      this.renderer.appendChild(document.body, script);
+    });
   }
 }
